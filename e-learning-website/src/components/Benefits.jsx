b55bbb384c034f007df9214991e138955ac7bce5
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-01-24 14:26:43 +0700

    feat: add Title component and enhance Benefits section with dynamic content

--- e-learning-website/src/components/Benefits.jsx
+++ e-learning-website/src/components/Benefits.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
+import Title from "./Title.jsx";
+import {benefitItems} from "../constant/data.js";
+import {RiArrowRightUpLine} from "@remixicon/react";
 
 const Benefits = () => {
   return (
-    <section>
+    <section className="section">
       <div className="container">
         {/* Title */}
-
+        <Title title='Benefits' text='Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.' link='View All'/>
 
         {/* Card wrapper */}
+        <div className="">
+          {benefitItems.map((item) => (
+          // Card
+            <div className="" key={item.id}>
+              {/* Icon */}
+              <div className="">
+                <img src={item.icon} alt={item.title} width={64} height={64} />
+              </div>
+              {/* Content */}
+              <div className="">
+                <h4>{item.title}</h4>
+                <p>{item.text}</p>
+              </div>
 
+              {/* Button */}
+              <button>
+                <RiArrowRightUpLine size={26}/>
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- e-learning-website/src/components/Title.jsx
+++ e-learning-website/src/components/Title.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Title = () => {
+const Title = ({title, text, link}) => {
   return (
     <div>
+      <div>
+        <h2>{title}</h2>
+        <p>{text}</p>
+      </div>
 
+      <button className="secondary-btn">{link}</button>
     </div>
   );
 };
