12d1e97303f9bd56615ffb234e5a5dee31998347
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-01-24 13:55:03 +0700

    feat: add Benefits and Title components to enhance application structure

---
+++ e-learning-website/src/components/Benefits.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Benefits = () => {
+  return (
+    <section>
+      <div className="container">
+        {/* Title */}
+
+
+        {/* Card wrapper */}
+
+      </div>
+    </section>
+  );
+};
+
+export default Benefits;
