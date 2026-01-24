abb916a46f0af0d30082573f0d16165c2ecac542
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-01-24 19:54:20 +0700

    feat: add Courses component to display course offerings

--- e-learning-website/src/App.jsx
+++ e-learning-website/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 import Benefits from "./components/Benefits.jsx";
+import Courses from "./components/Courses.jsx";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <main>
         <Hero />
         <Benefits />
+        <Courses />
       </main>
     </>
   );
