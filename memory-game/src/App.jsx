0432899a48063586cfcd385154fb22bfbe8adef2
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-02-07 18:05:30 +0700

    feat: implement card flipping logic; add state management for flipped and matched cards

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -23,6 +23,8 @@ const cardValue = [
 
 const App = () => {
   const [cards, setCards] = useState([]);
+  const [flippedCards, setFlippedCards] = useState([]);
+  const [matchedCard, setMatchCard] = useState([]);
 
   const initializeGame = () => {
     // Shuffle the cards
@@ -41,13 +43,71 @@ const App = () => {
     initializeGame();
   }, []);
 
+  const handleFlip = (card) => {
+    // Don't allow clicking if card is already flipped or already matched
+    if (card.isFlipped || card.isMatched) {
+      return;
+    }
+
+    // Update card flipped state
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return { ...c, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards);
+
+    const newFlippedCards = [...flippedCards, card.id];
+    setFlippedCards(newFlippedCards);
+
+    // Check for match if two cards are flipped
+    if (flippedCards.length === 1) {
+      const firstCard = cards[flippedCards[0]];
+
+      if (firstCard.value === card.value) {
+        setTimeout(() => {
+          setMatchCard((prev) => [...prev, firstCard.id, card.id]);
+
+          setCards((prev) =>
+            prev.map((c) => {
+              if (c.id === card.id || c.id === firstCard.id) {
+                return { ...c, isMatched: true };
+              } else {
+                return c;
+              }
+            }),
+          );
+          setFlippedCards([]);
+        }, 500);
+      } else {
+        // flip back card 1 and card 2
+        setTimeout(() => {
+          const flippedBackCard = newCards.map((c) => {
+            if (newFlippedCards.includes(c.id) || c.id === card.id) {
+              return { ...c, isFlipped: false };
+            } else {
+              return c;
+            }
+          });
+
+          setCards(flippedBackCard);
+
+          setFlippedCards([]);
+        }, 1000);
+      }
+    }
+  };
+
   return (
     <div className="app">
       <GameHeader score={3} moves={2} />
 
       <div className="cards-grid">
         {cards.map((item) => (
-          <Card card={item} />
+          <Card card={item} flip={handleFlip} />
         ))}
       </div>
     </div>
--- memory-game/src/components/Card.jsx
+++ memory-game/src/components/Card.jsx
@@ -1,6 +1,9 @@
-const Card = ({ card }) => {
+const Card = ({ card, flip }) => {
   return (
-    <div className="card">
+    <div
+      className={`card ${card.isFlipped ? "flipped" : ""}`}
+      onClick={() => flip(card)}
+    >
       <div className="card-front">?</div>
       <div className="card-back">{card.value}</div>
     </div>
