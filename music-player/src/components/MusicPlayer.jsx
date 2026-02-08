7e2218bf4d72acd20151d3fb05611a331c2069d1
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-02-08 13:30:42 +0700

    feat: enhance MusicPlayer component; add audio controls, progress tracking, and time formatting

--- music-player/src/components/MusicPlayer.jsx
+++ music-player/src/components/MusicPlayer.jsx
@@ -1,14 +1,79 @@
 import { useMusic } from "../hooks/use-music.js";
+import { useEffect, useRef } from "react";
+import { StepBack, StepForward, Play } from "lucide-react";
 
 const MusicPlayer = () => {
-  const { currentTrack } = useMusic();
+  const {
+    currentTrack,
+    formatTime,
+    currentTime,
+    setCurrentTime,
+    duration,
+    setDuration,
+    prevTrack,
+    nextTrack,
+  } = useMusic();
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    const audio = audioRef.current;
+
+    if (!audio) return;
+
+    const handleLoadedMetadata = () => {
+      setDuration(audio.duration);
+      console.log(audio.duration);
+    };
+
+    const handleTimeUpdate = () => {};
+
+    const handleEnded = () => {};
+
+    audio.addEventListener("loadedmetadata", handleLoadedMetadata);
+
+    return () => {
+      audio.removeEventListener("loadedmetadata", handleLoadedMetadata);
+    };
+  }, [setDuration, setCurrentTime, currentTrack]);
 
   return (
     <div className="music-player">
-      <audio />
+      <audio
+        ref={audioRef}
+        src={currentTrack.url}
+        preload="metadata"
+        crossOrigin="anonymous"
+      />
 
       <div className="track-info">
-        <h3>{currentTrack.artist}</h3>
+        <h3 className="track-title">{currentTrack.title}</h3>
+        <p className="track-artist">{currentTrack.artist}</p>
+      </div>
+
+      <div className="progress-container">
+        <span className="time">{formatTime(currentTime)}</span>
+        <input
+          type="range"
+          min="0"
+          max={duration || 0}
+          step="0.1"
+          value={currentTime || 0}
+          className="progress-bar"
+          style={{}}
+        />
+        <span className="time">{formatTime(duration)}</span>
+      </div>
+
+      <div className="controls">
+        <button className="control-btn" onClick={prevTrack}>
+          <StepBack />
+        </button>
+        <button className="control-btn play-btn">
+          <Play />
+        </button>
+        <button className="control-btn" onClick={nextTrack}>
+          <StepForward />
+        </button>
       </div>
     </div>
   );
--- music-player/src/hooks/use-music.js
+++ music-player/src/hooks/use-music.js
@@ -63,11 +63,52 @@ export const useMusic = () => {
   const [allSongs, setAllSongs] = useState(songs);
   const [currentTrack, setCurrentTrack] = useState(songs[0]);
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
 
   const handlePlaySong = (song, index) => {
     setCurrentTrack(song);
     setCurrentTrackIndex(index);
   };
 
-  return { allSongs, handlePlaySong, currentTrack, currentTrackIndex };
+  const nextTrack = () => {
+    setCurrentTrackIndex((prev) => {
+      const nextIndex = (prev + 1) % allSongs.length;
+
+      setCurrentTrack(allSongs[nextIndex]);
+      return nextIndex;
+    });
+  };
+
+  const prevTrack = () => {
+    setCurrentTrackIndex((prev) => {
+      const prevIndex = prev === 0 ? allSongs.length - 1 : prev - 1;
+
+      setCurrentTrack(allSongs[prevIndex]);
+      return prevIndex;
+    });
+  };
+
+  const formatTime = (time) => {
+    if (isNaN(time) || undefined) return "0:00";
+
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+  };
+
+  return {
+    allSongs,
+    handlePlaySong,
+    currentTrack,
+    currentTrackIndex,
+    currentTime,
+    setCurrentTime,
+    formatTime,
+    duration,
+    setDuration,
+    nextTrack,
+    prevTrack,
+  };
 };
